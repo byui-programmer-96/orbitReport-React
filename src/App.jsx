4f7eb8a126a98ab37b233e57aecdb935612b5314
byui-programmer-96 <bam21002@byui.edu>
2024-08-30 14:45:39 -0500

    comitting because I haven't in awhile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
+import satData from "./components/satData";
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 
-
 function App() {
+  const [sat, setSat] = useState(satData);
+
+  const filterByType = (currentType) => {
+    const filteredSats = satData.filter((satellite) => satellite.orbitType === currentType);
+    setSat(filteredSats);
+  };
+
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons filterByType={filterByType} setSat={setSat} satData={satData} />
+      <Table sat={sat} />
     </div>
   );
 }
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,26 @@
+import React, { useState } from 'react';
+//import satData from './components/satData.jsx'
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  const handleFilterClick = (orbitType) => {
+    filterByType(orbitType);
+  };
+
+  const handleResetClick = () => {
+    setSat(null);
+    displaySats();
+  };
+
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="button-container">
+      <button onClick={() => handleFilterClick('LEO')}>Low Earth Orbit</button>
+      <button onClick={() => handleFilterClick('MEO')}>Medium Earth Orbit</button>
+      <button onClick={() => handleFilterClick('GEO')}>Geostationary Orbit</button>
+      <button onClick={handleResetClick}>All Orbits</button>
     </div>
+    
   );
 };
 
 export default Buttons;
+
